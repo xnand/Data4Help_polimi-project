caa881587c1e2e3ab1e131b2839104801244d4e1
Carlo <carlo.pulvirenti@mail.polimi.it>
2019-01-04 16:39:28 +0100

    validation form added

---
+++ ITD/WebServer/public/validationForm.js
@@ -0,0 +1,45 @@
+function Modulo() {
+    // Variabili associate ai campi del modulo
+    var businessName = document.modulo.businessName.value;
+    var vat = document.modulo.vat.value;
+    var email = document.modulo.email.value;
+    var message = document.modulo.message.value;
+
+    var email_reg_exp = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-]{2,})+.)+([a-zA-Z0-9]{2,})+$/;
+
+
+    if ((businessName == "") || (businessName == "undefined")||(businessName == "Business Name")) {
+        alert("All fields are mandatory.");
+        document.modulo.businessName.focus();
+        return false;
+    }
+
+    else if ((vat == "") || (vat == "undefined")||(vat == "Vat")) {
+        alert("All fields are mandatory.");
+        document.modulo.vat.focus();
+        return false;
+    }
+
+    else if ((email == "") || (email == "undefined")||(email == "Email")) {
+        alert("All fields are mandatory.");
+        document.modulo.email.focus();
+        return false;
+    }
+
+    else if ((message == "") || (message == "undefined")||(message == "Tell us whay you want to use our services.")) {
+        alert("All fields are mandatory.");
+        document.modulo.message.focus();
+        return false;
+    }
+
+    else if (!email_reg_exp.test(email)) {
+        alert("Email not valid.");
+        document.modulo.email.select();
+        return false;
+    }
+
+    else {
+        document.modulo.action = "/send";
+        document.modulo.submit();
+    }
+}
